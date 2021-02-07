6438e6e9147f176225e0ba863a504dd8756c6805
Michal Galet <michal.galet@gmail.com>
2021-02-07 21:18:52 +0100

    Fixing case when no tags are used and adding option to run in Docker.
    Minor improvements:
     - Fixed a bug for cases when no tags are used - no ';' char at the end, numeric value not parsed correctly
     - Added logging
     - Dockerfile and guide how to run as Docker container
     - Code reformatted to conform to Python conventions, fixed mixed tabs ans spaces

--- Loxone2InfluxDB.py
+++ Loxone2InfluxDB.py
@@ -1,28 +1,12 @@
 #!/usr/bin/python
 
 # Simple script to import Loxone UDP logs into InfluxDB
-
-# hostname and port of InfluxDB http API
-host = '127.0.0.1'
-port = 8086
-# use https for connection to InfluxDB
-ssl = False
-# verify https connection
-verify = False
-# InfluxDB database name
-dbname = 'loxone'
-# InfluxDB login credentials (optional, specify if you have enabled authentication)
-dbuser = 'some_user'
-dbuser_code = 'some_password'
-# local IP and port where the script is listening for UDP packets from Loxone
-localIP = '192.168.1.222'
-localPort = 2222
-
-
+import os
 import socket
 import json
 import argparse
 import re
+import logging
 from influxdb import InfluxDBClient
 from datetime import datetime
 from dateutil import tz
@@ -30,112 +14,142 @@ from dateutil import tz
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Parse received message
-# Syntax: <timestamp>;<measurement_name>;<alias(optional)>:<value>;<tag_1(optional)>;<tag_2(optional)>;<tag_3(optional)>
-# Example: "2020-09-10 19:46:20;Bedroom temperature;23.0"
-# ** TO DO ** Need to add checks in case something goes wrong
-#
-def ParseLogData (data, from_zone, to_zone, debug=False):
-	if debug == True:
-		print ('-------------------------------------------------')
-		print (data)
-	EndTimestamp  = data.find(b';')
-	EndName       = data.find(b';', EndTimestamp+1)
-	EndAlias = data.find(b':', EndName+1)
-	if EndAlias < 0:		# -1 means not found
-		EndAlias = EndName
-        EndValue      = data.find(b';', EndAlias+1)
-        if EndValue > 0:
-                EndTag1 = data.find(b';', EndValue+1)
-        else:
-		EndTag1 = 0
-        if EndTag1 > 0:
-		EndTag2 = data.find(b';', EndTag1+1)
-	else:
-		EndTag2 = 0
-        if EndTag2 > 0:
-                EndTag3 = data.find(b';', EndTag2+1)
-	else:
-                EndTag3 = 0
-	numeric_const_pattern = '[-+]? (?: (?: \d* \. \d+ ) | (?: \d+ \.? ) )(?: [Ee] [+-]? \d+ ) ?'
-	rx = re.compile(numeric_const_pattern, re.VERBOSE)
-	ParsedData = {}
-	
-	# Timestamp Extraction
-	ParsedData['TimeStamp']=data[0:EndTimestamp]
-	ParsedData['TimeStamp']=ParsedData['TimeStamp'].replace(b' ',b'T')+b'Z'
-	# Timezone conversion to UTC
-	local = datetime.strptime(ParsedData['TimeStamp'].decode('utf-8'), b'%Y-%m-%dT%H:%M:%SZ'.decode('utf-8'))
-	local = local.replace(tzinfo=from_zone)
-	UTC = local.astimezone(to_zone)
-	ParsedData['TimeStamp'] = UTC.strftime('%Y-%m-%dT%H:%M:%SZ')
-	
-	# Name Extraction
-	ParsedData['Name']=data[EndTimestamp+1:EndName]
-	
-        # Alias Extraction
-        if EndAlias != EndName:
-		ParsedData['Name']=data[EndName+1:EndAlias]
+# =============== Configuration =====================
+# hostname and port of InfluxDB http API
+host = os.getenv('INFLUXDB_HOST', '127.0.0.1')
+port = int(os.getenv('INFLUXDB_PORT', 8086))
+# use https for connection to InfluxDB
+ssl = bool(os.getenv('INFLUXDB_HTTPS', False))
+# verify https connection
+verify = bool(os.getenv('INFLUXDB_VERIFY_HTTPS', False))
+# InfluxDB database name
+dbname = os.getenv('INFLUXDB_DATABASE', 'loxone')
+# InfluxDB login credentials (optional, specify if you have enabled authentication)
+dbuser = os.getenv('INFLUXDB_USER')
+dbuser_code = os.getenv('INFLUXDB_PASSWORD')
+# local IP and port where the script is listening for UDP packets from Loxone
+localIP = os.getenv('BIND_ADDRESS', '0.0.0.0')
+localPort = int(os.getenv('BIND_PORT', 2222))
 
-	# Value Extraction
-	ParsedData['Value']=rx.findall(data[EndAlias+1:EndValue].decode('utf-8'))[0]
-	
-        #Tag_1 Extraction
-        ParsedData['Tag_1']=data[EndValue+1:EndTag1].rstrip()
 
-        #Tag_2 Extraction
-	ParsedData['Tag_2']=data[EndTag1+1:EndTag2].rstrip()
+def parse_log_data(data, from_zone, to_zone, debug=False):
+    """
+    Parse received message
+    Syntax: <timestamp>;<measurement_name>;<alias(optional)>:<value>;<tag_1(optional)>;<tag_2(optional)>;<tag_3(optional)>
+    Example: "2020-09-10 19:46:20;Bedroom temperature;23.0"
+    ** TO DO ** Need to add checks in case something goes wrong
+    """
+    logging.debug('Received: %s', data)
 
-        #Tag_3 Extraction
-	ParsedData['Tag_3']=data[EndTag2+1:EndTag3].rstrip()
+    end_timestamp = data.find(b';')
+    end_name = data.find(b';', end_timestamp+1)
+    end_alias = data.find(b':', end_name+1)
+    if end_alias < 0:		# -1 means not found
+        end_alias = end_name
+    end_value = data.find(b';', end_alias+1)
 
-	#Create Json body for Influx
-	json_body = [
-		{
-			"measurement": ParsedData['Name'].decode('utf-8'),
-			"tags": {
-				"Tag_1": ParsedData['Tag_1'].decode('utf-8'),
-                                "Tag_2": ParsedData['Tag_2'].decode('utf-8'),
-                                "Tag_3": ParsedData['Tag_3'].decode('utf-8'),
-				"Source": "Loxone",
-			},
-			"time": ParsedData['TimeStamp'],   #  "2009-11-10T23:00:00Z",
-			"fields": {
-				"value": float(ParsedData['Value'])
-			}
-		}
-]                                    
-	if debug == True:
-		print (json.dumps(json_body, indent=4))
-		
-	return json_body;
+    if end_value < 0:  # ; char not found after value
+        end_tag1 = 0
+        end_value = len(data)
+    else:
+        end_tag1 = data.find(b';', end_value + 1)
+
+    if end_tag1 > 0:
+        end_tag2 = data.find(b';', end_tag1+1)
+    else:
+        end_tag2 = 0
+    if end_tag2 > 0:
+        end_tag3 = data.find(b';', end_tag2+1)
+    else:
+        end_tag3 = 0
+    numeric_const_pattern = r'[-+]? (?: (?: \d* \. \d+ ) | (?: \d+ \.? ) )(?: [Ee] [+-]? \d+ ) ?'
+    rx = re.compile(numeric_const_pattern, re.VERBOSE)
+
+    # Timestamp Extraction
+    parsed_data = {'TimeStamp': data[0:end_timestamp]}
+    parsed_data['TimeStamp'] = parsed_data['TimeStamp'].replace(b' ', b'T')+b'Z'
+    # Timezone conversion to UTC
+    local = datetime.strptime(parsed_data['TimeStamp'].decode('utf-8'), b'%Y-%m-%dT%H:%M:%SZ'.decode('utf-8'))
+    local = local.replace(tzinfo=from_zone)
+    utc = local.astimezone(to_zone)
+    parsed_data['TimeStamp'] = utc.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Name Extraction
+    parsed_data['Name'] = data[end_timestamp+1:end_name]
+
+    # Alias Extraction
+    if end_alias != end_name:
+        parsed_data['Name'] = data[end_name+1:end_alias]
+
+    # Value Extraction
+    parsed_data['Value'] = rx.findall(data[end_alias+1:end_value].decode('utf-8'))[0]
+
+    # Tag_1 Extraction
+    parsed_data['Tag_1'] = data[end_value+1:end_tag1].rstrip()
+
+    # Tag_2 Extraction
+    parsed_data['Tag_2'] = data[end_tag1+1:end_tag2].rstrip()
+
+    # Tag_3 Extraction
+    parsed_data['Tag_3'] = data[end_tag2+1:end_tag3].rstrip()
+
+    # Create Json body for Influx
+    json_body = [
+        {
+            "measurement": parsed_data['Name'].decode('utf-8'),
+            "tags": {
+                "Tag_1": parsed_data['Tag_1'].decode('utf-8'),
+                "Tag_2": parsed_data['Tag_2'].decode('utf-8'),
+                "Tag_3": parsed_data['Tag_3'].decode('utf-8'),
+                "Source": "Loxone",
+            },
+            "time": parsed_data['TimeStamp'],   # "2009-11-10T23:00:00Z",
+            "fields": {
+                "value": float(parsed_data['Value'])
+            }
+        }
+    ]
+
+    if debug:
+        logging.debug(json.dumps(json_body, indent=2))
+
+    return json_body
+
 
 def main(host, port, ssl, verify, debug=False):
-    ## """Instantiate a connection to the InfluxDB."""
+    """Instantiate a connection to the InfluxDB and stard listening on UDP port for incoming messages"""
+
+    logging.basicConfig(level=logging.DEBUG if debug else logging.INFO,
+                        format='%(asctime)s %(levelname)s - %(message)s')
+    logging.info('Creating InfluxDB client - connection: %s:%s, db: %s', host, port, dbname)
+
     client = InfluxDBClient(host, port, dbuser, dbuser_code, dbname, ssl, verify)
-	
+
     # get TZ info
     to_zone = tz.tzutc()
     from_zone = tz.tzlocal()
-    
+
     # A UDP server
     # Set up a UDP server
-    UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    logging.info('Listening for incoming Loxone UDP packets on %s:%s', localIP, localPort)
+    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Listen on local port
     # (to all IP addresses on this system)
-    listen_addr = (localIP,localPort)
-    UDPSock.bind(listen_addr)
+    listen_addr = (localIP, localPort)
+    udp_sock.bind(listen_addr)
+    logging.debug('Socket attached')
 
     # Report on all data packets received and
     # where they came from in each case (as this is
     # UDP, each may be from a different source and it's
     # up to the server to sort this out!)
     while True:
-        data,addr = UDPSock.recvfrom(1024)
-        json_body_log=ParseLogData(data,from_zone, to_zone,debug)
-		# Write to influx DB
+        data, addr = udp_sock.recvfrom(1024)
+        json_body_log = parse_log_data(data, from_zone, to_zone, debug)
+        # Write to influx DB
         client.write_points(json_body_log)
+
 
 def parse_args():
     """Parse the args."""
@@ -150,15 +164,13 @@ def parse_args():
                         help='use https to connect to InfluxDB')
     parser.add_argument('-v', '--verify', default=verify, action="store_true",
                         help='verify https connection to InfluxDB')
-    parser.add_argument('-d', '--debug', action="store_true",
+    parser.add_argument('-d', '--debug', action="store_true", default=bool(os.getenv('DEBUG', False)),
                         help='debug code')
     parser.add_argument('-?', '--help', action='help',
-    			help='show this help message and exit')
+                        help='show this help message and exit')
     return parser.parse_args()
-
 
 
 if __name__ == '__main__':
     args = parse_args()
     main(host=args.host, port=args.port, ssl=args.ssl, verify=args.verify, debug=args.debug)
-
